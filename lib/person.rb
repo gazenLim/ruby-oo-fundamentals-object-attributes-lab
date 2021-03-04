b310c62c2cb8a5a262c7ee8be44299f5ba41d880
gajen <meetgajen@yahoo.com>
2021-03-03 20:43:47 -0500

    commit tree b18aa3209faca52d35a4f06d86ecea9569a4128a

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+require 'pry'
+class Person 
+
+    #instance variable
+        def name=(name)
+            @name=name
+        end
+
+
+        def name
+            @name
+        end
+
+        def job=(job)
+            @job=job
+        end
+
+        def job
+            @job
+        end
+
+
+
+
+
+
+end
